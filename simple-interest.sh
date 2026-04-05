8f7a544b822f7ee333660fde4eaa0e70c955e6dc
AsifArkadia <kangasmaaara22@gmail.com>
2026-04-05 09:10:22 +0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
-# This script calculates simple interest given principal, annual rate of interest and time period in years.
-# Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
-# Addtional Authors:
-# <your Github username>
-
-# Input:
-# p, principal amount
-# t, time period in years
-# r, annual rate of interest
-
-# Output:
-# simple interest = p*t*r
-
-echo "Enter the principal:"
+echo "Masukkan pokok:"
 read p
-echo "Enter rate of interest per year:"
+
+echo "Masukkan suku bunga:"
 read r
-echo "Enter time period in years:"
+
+echo "Masukkan waktu:"
 read t
 
-s=$(expr $p \* $t \* $r / 100)
-echo "The simple interest is: "
-echo $s
+interest=$((p * r * t / 100))
+
+echo "Bunga sederhana: $interest"
